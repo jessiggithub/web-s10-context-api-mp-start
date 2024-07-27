ddab5b0f7abfc124235a58c28e0f158eb4c694f6
Jessica Garcia <164922787+jessiggithub@users.noreply.github.com>
2024-07-26 20:49:02 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Quotes from './Quotes'
+import QuoteForm from './QuoteForm'
+
+export default function App() {
+  return (
+    <div id="mp">
+      <h2>Module Project</h2>
+      <Quotes
+      />
+      <QuoteForm
+      />
+    </div>
+  )
+}
